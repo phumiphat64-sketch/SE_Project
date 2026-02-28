e8341562066813a36c64706de596c037c7862f57
Phumiphat <phumiphat64@gmail.com>
2026-02-28 15:46:29 +0700

    Prepare Database & Repositories for Backend Server

---
+++ src/domain/user.model.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: String,
+    password: String,
+  },
+  { timestamps: true },
+);
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
