b19dc4a682349149d90f29ab4f361b6aa10c61fd
Phumiphat <phumiphat64@gmail.com>
2026-03-08 03:09:37 +0700

    Inventory now 95% & add BinarySearch function in Search option

--- src/app/(protected)/seller/inventory/page.js
+++ src/app/(protected)/seller/inventory/page.js
@@ -2,11 +2,33 @@
 import styles from "./inventory.module.css";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { Crimson_Text, Caveat, Afacad, IBM_Plex_Mono } from "next/font/google";
+
+export const crimson = Crimson_Text({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
+
+export const caveat = Caveat({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
+
+export const afacad = Afacad({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+export const ibmPlexMono = IBM_Plex_Mono({
+  subsets: ["latin", "thai"],
+  weight: ["400", "500", "600", "700"],
+});
 
 export default function InventoryPage() {
     const router = useRouter();
   const [books, setBooks] = useState([]);
   const [page, setPage] = useState(1);
+  const [search, setSearch] = useState("");
 
   const getStatusClass = (status) => {
     if (status === "Published") return styles.published;
@@ -32,29 +54,74 @@ export default function InventoryPage() {
 
   const booksPerPage = 5;
 
+  const sortedBooks = [...books].sort((a, b) => a.title.localeCompare(b.title));
+
   const last = page * booksPerPage;
   const first = last - booksPerPage;
 
-  const currentBooks = books.slice(first, last);
+  const filteredBooks = search
+    ? binarySearch(sortedBooks, search.toLowerCase())
+    : sortedBooks;
 
-  const totalPages = Math.ceil(books.length / booksPerPage);
+  const currentBooks = filteredBooks.slice(first, last);
+
+  const totalPages = Math.ceil(filteredBooks.length / booksPerPage);
+
+  function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    const results = [];
+
+    while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+      let title = arr[mid].title.toLowerCase();
+
+      if (title.includes(target)) {
+        results.push(arr[mid]);
+
+        let l = mid - 1;
+        while (l >= 0 && arr[l].title.toLowerCase().includes(target)) {
+          results.push(arr[l]);
+          l--;
+        }
+
+        let r = mid + 1;
+        while (r < arr.length && arr[r].title.toLowerCase().includes(target)) {
+          results.push(arr[r]);
+          r++;
+        }
+
+        break;
+      }
+
+      if (title < target) left = mid + 1;
+      else right = mid - 1;
+    }
+
+    return results;
+  }
 
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
+      <div className={`${styles.header} ${afacad.className}`}>
         <h2>All Books</h2>
 
         <button
-          className={styles.addBtn}
+          className={`${styles.addBtn} ${afacad.className}`}
           onClick={() => router.push("/seller/addbooks")}
         >
           + Add New Book
         </button>
       </div>
 
-      <input className={styles.search} placeholder="Search Book..." />
+      <input
+        className={`${styles.search} ${afacad.className}`}
+        placeholder="Search Book..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
 
-      <div className={styles.tableContainer}>
+      <div className={`${styles.tableContainer} ${afacad.className}`}>
         <table className={styles.table}>
           <thead>
             <tr>
@@ -95,9 +162,10 @@ export default function InventoryPage() {
 
                 <td>
                   <button
-                    className={styles.editBtn}
+                    className={`${styles.editBtn} ${afacad.className}`}
                     onClick={() => router.push(`/seller/editbook/${book._id}`)}
                   >
+                    <img src="/icons/pen.svg" className={styles.editIcon} />
                     Edit
                   </button>
                 </td>
@@ -107,7 +175,7 @@ export default function InventoryPage() {
         </table>
       </div>
 
-      <div className={styles.pagination}>
+      <div className={`${styles.pagination} ${afacad.className}`}>
         Page {page} of {totalPages}
         <button disabled={page === 1} onClick={() => setPage(page - 1)}>
           {"<"}
