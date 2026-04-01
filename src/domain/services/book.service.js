e973faf67cc214bd1081182b074f18b5f8e38eda
Phumiphat <phumiphat64@gmail.com>
2026-04-01 21:23:28 +0700

    Refactoring Book.service to be Dependency Injection + Service Pattern

--- src/domain/services/book.service.js
+++ src/domain/services/book.service.js
@@ -1,14 +1,15 @@
 import Book from "../entities/book.entity";
 
+//Dependency Injection + Service Pattern
+
 export default class BookService {
   constructor(bookRepository) {
     this.bookRepository = bookRepository;
   }
 
   async addBook(data) {
-    const book = new Book(data);
-
-    return await this.bookRepository.addBook(book);
+    // 🔹 ยุบรวมการสร้าง Entity (new Book) และส่งให้ Repository ไว้ในบรรทัดเดียว
+    return await this.bookRepository.addBook(new Book(data));
   }
 
   async updateBook(bookId, data) {
