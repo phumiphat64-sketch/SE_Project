7e8c30847384a7b60ae59fe983f431f1205176d1
Phumiphat <phumiphat64@gmail.com>
2026-04-01 21:37:09 +0700

    Refactoring book.repository.interface to be Repository Pattern + Dependency Inversion Principle

--- src/domain/repositories/book.repository.interface.js
+++ src/domain/repositories/book.repository.interface.js
@@ -1,17 +1,21 @@
+// domain/repositories/book.repository.interface.js
+// Repository Pattern + Dependency Inversion Principle
+// Repository Interface (Abstraction Layer)
+
 export default class BookRepository {
   async addBook(book) {
-    throw new Error("Not implemented");
+    throw new Error("Method 'addBook()' must be implemented");
   }
 
   async updateBook(bookId, data) {
-    throw new Error("Not implemented");
+    throw new Error("Method 'updateBook()' must be implemented");
   }
 
   async deleteBook(bookId) {
-    throw new Error("Not implemented");
+    throw new Error("Method 'deleteBook()' must be implemented");
   }
 
   async getBooksBySeller(sellerId) {
-    throw new Error("Not implemented");
+    throw new Error("Method 'getBooksBySeller()' must be implemented");
   }
 }
